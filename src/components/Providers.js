ec639681f386152964e802aa4c566b6989ebfcf1
Rafiul29 <rafiulislam4472@gmail.com>
2023-07-30 00:33:16 +0600

    about page added

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "next-themes";
 
-const Providers = ({children}) => {
+const Providers = ({ children }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-    <div className="drak:bg-slate-800 dark:text-slate-200 text-slate-800 transition-colors duration-300 min-h-screen select-none">
-    {children}
-    </div>
-      
+      <div className="drak:bg-slate-800 dark:text-slate-200 text-slate-800 transition-colors duration-300 min-h-screen select-none">
+        {children}
+      </div>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
